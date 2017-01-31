37df9f61310ecda48794a9f97778f1fac0472400
alex <45722053p@iespoblenou.org>
2017-01-31 16:08:09 +0100

    creando list view edit text,etc....

--- app/src/main/java/com/proyecto/todosfirebase/MainActivityFragment.java
+++ app/src/main/java/com/proyecto/todosfirebase/MainActivityFragment.java
@@ -1,15 +1,23 @@
 package com.proyecto.todosfirebase;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    ListView listaTodo;
+    EditText mensaje;
+    Button btnEnviar;
+
 
     public MainActivityFragment() {
     }
@@ -19,6 +27,10 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View viewFragment = inflater.inflate(R.layout.fragment_main, container, false);
 
+
+        listaTodo = (ListView) viewFragment.findViewById(R.id.listView);
+        mensaje = (EditText) viewFragment.findViewById(R.id.notaEnviar);
+        btnEnviar = (Button) viewFragment.findViewById(R.id.btn_Enviar);
 
 
 
